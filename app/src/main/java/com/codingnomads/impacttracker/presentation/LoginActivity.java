66f63987181f5bfea9a899f7925ffc43d88ef49a
Andy <andymitchell1@live.co.uk>
2018-12-31 14:50:15 +0000

    Added link to login page and improved login layout

--- app/src/main/java/com/codingnomads/impacttracker/presentation/LoginActivity.java
+++ app/src/main/java/com/codingnomads/impacttracker/presentation/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
